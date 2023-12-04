1876420bb4ab34457be0e47c686a1914d6309494
이승민 <dltmdals0608@naver.com>
2023-12-05 01:26:03 +0900

    [#4]Refactor: AddChild -> UIViewController+로 이름 변경
    - 필요없는 개행 삭제

--- Nagaza/Sources/Presentation/Utils/Extensions/UIView+.swift
+++ Nagaza/Sources/Presentation/Utils/Extensions/UIView+.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 extension UIView {
-  func addSubviews(_ views: [UIView]) {
-    views.forEach { self.addSubview($0) }
-  }
+    func addSubviews(_ views: [UIView]) {
+        views.forEach { self.addSubview($0) }
+    }
 }
 
