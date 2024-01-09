6b9d634da503c99a23592427ca2e5cb4f534bd99
LeeSeungmin <dltmdals0608@naver.com>
2024-01-09 20:20:33 +0900

    Update: 홈 화면 진입 시, 요청 및 요청에 대한 응답 데이터 받아와서 vc에 전달하는 코드추가
    - room 데이터를 받기 위해서는 cafeId를 알아야 하기 떄문에
    - cafes 데이터를 요청하고 room 데이터를 요청하는 방식으로 구현
    - enum view model 클래스 밖에 선언

--- Nagaza/Sources/Presentation/Feature/TabBar/Main/ViewModel/HomeViewModel.swift
+++ Nagaza/Sources/Presentation/Feature/TabBar/Main/ViewModel/HomeViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  MainViewModel.swift
+//  HomeViewModel.swift
 //  Nagaza
 //
 //  Created by 전성훈 on 2023/10/19.
@@ -7,59 +7,85 @@
 
 import Foundation
 
+import RxSwift
 import RxCocoa
 
 /// 화면 전환 등 액션, coordinator에서 직접 주입
-struct MainViewModelActions {
+struct HomeViewModelActions {
     let logoutTest: () -> Void
 }
 
-final class HomeViewModel: ViewModelType {
+enum ScrollOffsetState {
+    case top(value: Double)
+    case bottom
     
-    private let actions: MainViewModelActions!
-    
-    enum ScrollOffsetState {
-        case top(value: Double)
-        case bottom
-        
-        init(rawValue: Double) {
-            switch rawValue {
-            case -CGFloat.greatestFiniteMagnitude..<CGFloat.windowFrameheight / 3:
-                self = .top(value: rawValue)
-            default:
-                self = .bottom
-            }
-        }
-        
-        var alpha: Double {
-            switch self {
-            case .top(let value):
-                return value / (CGFloat.windowFrameheight / 3)
-            default:
-                return 1
-            }
+    init(rawValue: Double) {
+        switch rawValue {
+        case -CGFloat.greatestFiniteMagnitude..<CGFloat.windowFrameheight / 3:
+            self = .top(value: rawValue)
+        default:
+            self = .bottom
         }
     }
     
+    var alpha: Double {
+        switch self {
+        case .top(let value):
+            return value / (CGFloat.windowFrameheight / 3)
+        default:
+            return 1
+        }
+    }
+}
+
+// MARK: - HomeViewModel
+final class HomeViewModel: ViewModelType {
+    private let homeUseCase: HomeRepositoryInterface
+    private let actions: HomeViewModelActions!
+    
+    private var disposeBag = DisposeBag()
+    
     struct Input {
+        let initialTrigger: Driver<Void>
         let contentOffset: Driver<CGPoint>
     }
     
     struct Output {
+        let roomsList: Driver<[[Room]]>
         let scrollOffsetState: Driver<ScrollOffsetState>
     }
     
     init(
-        actions: MainViewModelActions
+        homeUseCase: HomeRepositoryInterface,
+        actions: HomeViewModelActions
     ) {
+        self.homeUseCase = homeUseCase
         self.actions = actions
     }
     
     func transform(input: Input) -> Output {
+        let cafesResponse = input.initialTrigger
+            .flatMapLatest { [unowned self] _ in
+                return homeUseCase.fetchCafesList().asDriver(onErrorJustReturn: .init(cafes: [], page: 0, totalPages: 0))
+            }
+
+        let roomsList = cafesResponse
+            .flatMapLatest { cafesPage in
+                if cafesPage.cafes.isEmpty { return Driver<[[Room]]>.just([]) }
+                else {
+                    let firstId = cafesPage.cafes[0].id
+                    return self.homeUseCase.fetchRoomsList(cafeId: firstId)
+                        .map { $0.roomsList }.asDriver(onErrorJustReturn: [])
+                }
+            }
+        
         let scrollOffsetState = input.contentOffset
             .map { ScrollOffsetState(rawValue: $0.y) }
         
-        return Output(scrollOffsetState: scrollOffsetState)
+        return Output(
+            roomsList: roomsList,
+            scrollOffsetState: scrollOffsetState
+        )
     }
 }
 
