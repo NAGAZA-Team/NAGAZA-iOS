083923feae87c45c7b507c44287183e3708d3159
LeeSeungmin <dltmdals0608@naver.com>
2024-02-20 15:45:43 +0900

    Fix: 빌드 에러 수정 - brace 추가

--- Nagaza/Sources/Domain/Entities/Region.swift
+++ Nagaza/Sources/Domain/Entities/Region.swift
@@ -58,62 +58,62 @@ enum Region: Int {
             return ["강원 전체", "강원"]
         case .jeju:
             return ["제주 전체", "제주"]
+        }
+        
+        //    enum Seoul: String {
+        //        case gangnam = "강남"
+        //        case kondae = "건대"
+        //        case hongdae = "홍대"
+        //        case sinchon = "신촌"
+        //        case daehakro = "대학로"
+        //        case gangbuk = "강북"
+        //        case sillim = "신림"
+        //        case others = "기타"
+        //    }
+        //
+        //    enum Gyenggi: String {
+        //        case incheon = "인천"
+        //        case bucheon = "부천"
+        //        case ilsand = "일산"
+        //        case suwon = "수원"
+        //        case anyang = "안양"
+        //        case others = "기타"
+        //    }
+        //
+        //    enum Chungcheong: String {
+        //        case daejeon = "대전"
+        //        case cheonan = "천안"
+        //        case cheongju = "청주"
+        //        case others = "기타"
+        //    }
+        //
+        //    enum Gyeongsang: String {
+        //          case daegu = "대구"
+        //          case busan = "부산"
+        //          case others = "기타"
+        //      }
+        //
+        //    enum Jeolla: String {
+        //        case gwangju = "광주"
+        //        case jeonju = "전주"
+        //        case others = "기타"
+        //    }
+        //
+        //    enum Gangwon: String {
+        //        case gangwon = "강원"
+        //    }
+        //
+        //    enum Jeju: String {
+        //        case jeju = "제주"
+        //    }
+        
+        //    /// 단순 Log - 삭제 예정
+        //    static func fullPath(main: Region, sub: String?) -> String {
+        //        if let subRegin = sub {
+        //            return "\(main.rawValue) -> \(subRegin)"
+        //        } else {
+        //            return main.rawValue
+        //        }
+        //    }
     }
-    
-//    enum Seoul: String {
-//        case gangnam = "강남"
-//        case kondae = "건대"
-//        case hongdae = "홍대"
-//        case sinchon = "신촌"
-//        case daehakro = "대학로"
-//        case gangbuk = "강북"
-//        case sillim = "신림"
-//        case others = "기타"
-//    }
-//    
-//    enum Gyenggi: String {
-//        case incheon = "인천"
-//        case bucheon = "부천"
-//        case ilsand = "일산"
-//        case suwon = "수원"
-//        case anyang = "안양"
-//        case others = "기타"
-//    }
-//    
-//    enum Chungcheong: String {
-//        case daejeon = "대전"
-//        case cheonan = "천안"
-//        case cheongju = "청주"
-//        case others = "기타"
-//    }
-//    
-//    enum Gyeongsang: String {
-//          case daegu = "대구"
-//          case busan = "부산"
-//          case others = "기타"
-//      }
-//    
-//    enum Jeolla: String {
-//        case gwangju = "광주"
-//        case jeonju = "전주"
-//        case others = "기타"
-//    }
-//    
-//    enum Gangwon: String {
-//        case gangwon = "강원"
-//    }
-//    
-//    enum Jeju: String {
-//        case jeju = "제주"
-//    }
-    
-//    /// 단순 Log - 삭제 예정
-//    static func fullPath(main: Region, sub: String?) -> String {
-//        if let subRegin = sub {
-//            return "\(main.rawValue) -> \(subRegin)"
-//        } else {
-//            return main.rawValue
-//        }
-//    }
 }
-
