8e33382a163c2f21a123bc56a193450d806c268c
LeeSeungmin <dltmdals0608@naver.com>
2024-02-21 09:31:11 +0900

    Feat: 맵 화면 UI 구현
    - searchTextField 구현
    - 처음에 권한을 설정하면 현재 위치를 가운데로 설정
    - 검색 버튼 클릭 이벤트를 input으로 생성
    - 오른쪽에 이미지 넣는 부분이 잘 안 되서 view에 subview add하고 frame 맞추는 방식으로 구현

--- Nagaza/Sources/Presentation/Feature/TabBar/Map/View/MapViewController.swift
+++ Nagaza/Sources/Presentation/Feature/TabBar/Map/View/MapViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 import MapKit
 
+import RxSwift
+import RxCocoa
+import RxGesture
+
 final class MapViewController: NagazaBaseViewController {
-    let mapView = MapView()
-    
     private var viewModel: MapViewModel!
+    
+    lazy var mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.showsUserLocation = true
+        return mapView
+    }()
+    
+    lazy var searchTextField: UISearchTextField = {
+        let searchTextField = UISearchTextField()
+        
+        let customView = UIView(frame: CGRect(x: -16, y: 0, width: 24, height: 24))
+        let searchImageView = UIImageView()
+        searchImageView.image = NagazaAsset.Images.imgSearch.image
+        customView.addSubview(searchImageView)
+        searchImageView.frame = customView.frame
+        
+        searchTextField.leftView = nil
+        searchTextField.rightView = customView
+        searchTextField.rightViewMode = .always
+        searchTextField.clearButtonMode = .never
+        searchTextField.placeholder = "검색어를 입력하세요"
+        searchTextField.backgroundColor = .white
+        searchTextField.isOpaque = true
+        searchTextField.alpha = 1
+        return searchTextField
+    }()
     
     static func create(with viewModel: MapViewModel) -> MapViewController {
         let vc = MapViewController()
@@ -26,6 +54,15 @@ final class MapViewController: NagazaBaseViewController {
         view = mapView
     }
     
+    override func makeUI() {
+        view.addSubview(searchTextField)
+        searchTextField.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).inset(25)
+            $0.leading.trailing.equalToSuperview().inset(13)
+            $0.height.equalTo(60)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupDelegate()
@@ -33,21 +70,44 @@ final class MapViewController: NagazaBaseViewController {
     }
     
     override func bindViewModel() {
-        let viewWillAppearTrigger = self.rx.viewWillAppear.map { _ in }.asDriverOnErrorJustEmpty()
+        guard let searchImageView = self.searchTextField.rightView else { return }
         
-        let input = MapViewModel.Input(viewWillAppearTrigger: viewWillAppearTrigger)
+        let searchButtonTapTrigger = searchImageView.rx.tapGesture()
+            .withLatestFrom(searchTextField.rx.text.map({ $0 ?? "" })) .asDriver(onErrorJustReturn: "")
+        
+        let input = MapViewModel.Input(searchButtonTapTrigger: searchButtonTapTrigger)
         
         let output = viewModel.transform(input: input)
         
-        output.maps
-            .drive()
+        output.coordinates
+            .do(onNext: { coordinates in
+                guard let value = coordinates else {
+                    // TODO: 입력한 값이 없을 때 처리
+                    print("검색한 값이 없음!!")
+                    return
+                }
+            })
+            .map { [weak self] value in
+                guard let self = self else { return MKCoordinateRegion() }
+                guard let coordinates = value else { return self.mapView.region }
+                return MKCoordinateRegion(
+                    center:
+                        CLLocationCoordinate2D(
+                            latitude: coordinates.latitude,
+                            longitude: coordinates.longitude
+                        ),
+                    latitudinalMeters: 500,
+                    longitudinalMeters: 500
+                )
+            }
+            .drive(mapView.rx.region)
             .disposed(by: disposeBag)
     }
 }
 
 extension MapViewController {
     private func setupDelegate() {
-        mapView.mapView.delegate = self
+        mapView.delegate = self
     }
     
     private func locationManagerDidChangeAuthorization() {
@@ -55,21 +115,22 @@ extension MapViewController {
         
         switch manager.authorizationStatus {
         case .authorizedWhenInUse:  // Location services are available.
-//            enableLocationFeatures()
-            print("authorizedWhenInUse")
+            //            enableLocationFeatures()
+            print("Authorized When In Use!")
             break
             
         case .restricted, .denied:  // Location services currently unavailable.
-//            disableLocationFeatures()
-            print("restricted or denied")
+            //            disableLocationFeatures()
+            print("Restricted or Denied!")
             break
             
         case .notDetermined:        // Authorization not determined yet.
             manager.requestWhenInUseAuthorization()
-            print("notDetermined")
+            print("Not Determined!")
             break
             
         default:
+            print("Else!")
             break
         }
     }
@@ -77,9 +138,11 @@ extension MapViewController {
 
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
-        let region = MKCoordinateRegion(center: userLocation.coordinate,
-                                        latitudinalMeters: 500,
-                                        longitudinalMeters: 500)
+        let region = MKCoordinateRegion(
+            center: userLocation.coordinate,
+            latitudinalMeters: 500,
+            longitudinalMeters: 500
+        )
         mapView.region = region
     }
 }
