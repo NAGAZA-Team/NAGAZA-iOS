c2fd7f5a0158cb87ca9e3158e2662a92e5bcd6bb
LeeSeungmin <dltmdals0608@naver.com>
2024-03-14 16:48:52 +0900

    [#22]Feat: 최근 검색 API 연동 구현
    - entity, use case response type, repository
    - use case interface와 repository interface를 나누기로 했었는데 추후에 할 예정

---
+++ Nagaza/Sources/Data/Repositories/Map/MapRepository.swift
@@ -0,0 +1,28 @@
+//
+//  MapRepository.swift
+//  Nagaza
+//
+//  Created by SeungMin on 3/14/24.
+//
+
+import RxSwift
+import Moya
+
+final class MapRepository: ProviderProtocol {
+    typealias Target = MapTarget
+    var provider: MoyaProvider<Target>
+    
+    init(isStub: Bool, sampleStatusCode: Int, customEndpointClosure: ((Target) -> Moya.Endpoint)?) {
+        self.provider = Self.consProvider(isStub, sampleStatusCode, customEndpointClosure)
+    }
+}
+
+extension MapRepository: MapRepositoryInterface {
+    func fetchRecentKeywordList() -> Single<RecentKeywordPage> {
+        request(
+            type: RecentKeywordResponse.self,
+            target: .fetchRecentKeywordList
+        )
+        .map { $0.toDomain() }
+    }
+}
