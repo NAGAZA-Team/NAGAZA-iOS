d4987ae71de304a9b9701d123920de1b0288236b
jeonseonghun <jsh097610@gmail.com>
2024-04-10 22:12:59 +0900

    [Chore]

--- Nagaza/Sources/Application/DIRegister.swift
+++ Nagaza/Sources/Application/DIRegister.swift
@@ -15,6 +15,7 @@ final class DIRegister {
     func registerDIContainer() {
         let container = DIContainer.shared
         
-        
+        container.register(<#T##type: T.Type##T.Type#>, dependency: <#T##T#>)
+        container.resolve(<#T##type: T.Type##T.Type#>)
     }
 }
